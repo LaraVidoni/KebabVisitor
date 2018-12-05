38168a51a4d9378b8cc09baddfd7233eca03748b
Lara <lv.laravidoni@gmail.com>
2018-12-05 11:05:22 +0100

    arborescence visiteur de régime + vdrVegetarien

---
+++ KebabVisitor/src/main/java/fr/unilim/iut/visitor/VisiteurDeRegimeVegetarien.java
@@ -0,0 +1,60 @@
+package fr.unilim.iut.visitor;
+
+import fr.unilim.iut.kebab.Assiette;
+import fr.unilim.iut.kebab.ingredients.Agneau;
+import fr.unilim.iut.kebab.ingredients.Boeuf;
+import fr.unilim.iut.kebab.ingredients.Crevette;
+import fr.unilim.iut.kebab.ingredients.Fromage;
+import fr.unilim.iut.kebab.ingredients.Oignon;
+import fr.unilim.iut.kebab.ingredients.Pain;
+import fr.unilim.iut.kebab.ingredients.Salade;
+import fr.unilim.iut.kebab.ingredients.Sauce;
+import fr.unilim.iut.kebab.ingredients.Thon;
+import fr.unilim.iut.kebab.ingredients.Tomate;
+
+public class VisiteurDeRegimeVegetarien implements VisiteurDeRegime {
+
+	public boolean visit(Agneau agneau) {
+		return false;
+	}
+
+	public boolean visit(Boeuf boeuf) {
+		return false;
+	}
+
+	public boolean visit(Crevette crevette) {
+		return false;
+	}
+
+	public boolean visit(Fromage fromage) {
+		return true;
+	}
+
+	public boolean visit(Oignon oignon) {
+		return true;
+	}
+
+	public boolean visit(Pain pain) {
+		return true;
+	}
+
+	public boolean visit(Salade salade) {
+		return true;
+	}
+
+	public boolean visit(Sauce sauce) {
+		return true;
+	}
+
+	public boolean visit(Thon thon) {
+		return false;
+	}
+
+	public boolean visit(Tomate tomate) {
+		return true;
+	}
+
+	public boolean visit(Assiette assiette) {
+		return true;
+	}
+}
