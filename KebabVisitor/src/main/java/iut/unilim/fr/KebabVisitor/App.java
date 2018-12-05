cfa3115a71f68eb600e6f8b12b19084c9d1df560
Lara <lv.laravidoni@gmail.com>
2018-12-05 10:19:22 +0100

    Initial commit

---
+++ KebabVisitor/src/main/java/iut/unilim/fr/KebabVisitor/App.java
@@ -0,0 +1,13 @@
+package iut.unilim.fr.KebabVisitor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
